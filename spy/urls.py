397963dea7231a79da4e8142f082bacb9640c6b9
Tom Manville <manville@dropbox.com>
2016-09-08 03:06:05 +0000

    [models] use viewsets and routers

--- spy/urls.py
+++ spy/urls.py
@@ -1,16 +1,15 @@
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
+
+router = DefaultRouter()
+router.register(r'players', views.PlayerViewSet)
+router.register(r'games', views.GameViewSet)
+router.register(r'missions', views.MissionViewSet)
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^api/v1/player/$', views.player_list),
-    url(r'^api/v1/players/(?P<pk>[0-9]+)$', views.player_detail),
-    url(r'^api/v1/game/$', views.game_list),
-    url(r'^api/v1/games/(?P<pk>[0-9]+)$', views.game_detail),
-    url(r'^api/v1/mission/$', views.mission_list),
-    url(r'^api/v1/missions/(?P<pk>[0-9]+)$', views.mission_detail),
+    url(r'^api/v1/', include(router.urls)),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
--- spy/views.py
+++ spy/views.py
@@ -1,89 +1,30 @@
 from django.shortcuts import render
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-from spy.models import Player, Game, Mission
-from spy.serializers import PlayerSerializer, GameSerializer, MissionSerializer
+from spy import models, serializers
 
 def index(request):
     template = 'spy/index.html'
     context = {}
     return render(request, template, context)
 
-def _list(request, cls, cls_serializer):
-    if request.method == 'GET':
-        objects = cls.objects.all()
-        serializer = cls_serializer(objects, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = cls_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-def _detail(request, pk, cls, cls_serializer):
-    try:
-        obj = cls.objects.get(pk=pk)
-    except cls.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = cls_serializer(obj)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = cls_serializer(obj, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        obj.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-@api_view(['GET', 'POST'])
-def player_list(request):
+class PlayerViewSet(ModelViewSet):
     """
-    List all players, or create a new player.
+    A viewset for viewing and editing player instances.
     """
-    return _list(request, Player, PlayerSerializer)
+    serializer_class = serializers.PlayerSerializer
+    queryset = models.Player.objects.all()
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def player_detail(request, pk):
+class GameViewSet(ModelViewSet):
     """
-    Retrieve, update or delete a player instance.
+    A viewset for viewing and editing game instances.
     """
-    return _detail(request, pk, Player, PlayerSerializer)
+    serializer_class = serializers.GameSerializer
+    queryset = models.Game.objects.all()
 
-@api_view(['GET', 'POST'])
-def game_list(request):
+class MissionViewSet(ModelViewSet):
     """
-    List all games, or create a new game.
+    A viewset for viewing and editing mission instances.
     """
-    return _list(request, Game, GameSerializer)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def game_detail(request, pk):
-    """
-    Retrieve, update or delete a game instance.
-    """
-    return _detail(request, pk, Game, GameSerializer)
-
-@api_view(['GET', 'POST'])
-def mission_list(request):
-    """
-    List all missions, or create a new mission.
-    """
-    return _list(request, Mission, MissionSerializer)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def mission_detail(request, pk):
-    """
-    Retrieve, update or delete a mission instance.
-    """
-    return _detail(request, pk, Mission, MissionSerializer)
+    serializer_class = serializers.MissionSerializer
+    queryset = models.Mission.objects.all()
